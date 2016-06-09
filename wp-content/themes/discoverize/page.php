f858ff2a0458797bb6a0253d71b59838a06e5fb0
andrej <weitergehen@gmail.com>
2016-06-09 13:53:13 +0200

    remove node_modules folder from discoverize theme

--- wp-content/themes/discoverize/page.php
+++ wp-content/themes/discoverize/page.php
@@ -20,7 +20,7 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'page-no-header' );
 
 				// comments turned off for now
 				// If comments are open or we have at least one comment, load up the comment template.
