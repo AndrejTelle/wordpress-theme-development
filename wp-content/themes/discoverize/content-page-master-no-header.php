1ae37e6fc52b4fe04e2a8a706e3828fb6047b92b
andrej <weitergehen@gmail.com>
2016-06-09 16:46:07 +0200

    no header page

--- wp-content/themes/discoverize/content-page-master-no-header.php
+++ wp-content/themes/discoverize/content-page-master-no-header.php
@@ -1,13 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * Template Name: No Header Page
  *
  * @package discoverize
  */
@@ -20,7 +13,7 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'page-no-header' );
 
 				// comments turned off for now
 				// If comments are open or we have at least one comment, load up the comment template.
